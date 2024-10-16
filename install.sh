63228ee604410be0ad82ac15efab53e475f14781
Aria Fatah <ariafatah0711@gmail.com>
2024-10-16 19:30:22 +0700

    update

--- install.sh
+++ install.sh
@@ -5,14 +5,23 @@ TARGET_PATH="/usr/local/bin"
 CONFIG_DIR="/etc/ngrok-run"
 SOURCE_DIR="$(pwd)"
 
+add_profile_to_bashrc() {
+    if ! grep -Fxq "source ~/.profile.sh" ~/.bashrc; then
+        echo "Adding source ~/.profile.sh to ~/.bashrc"
+        echo "source ~/.profile.sh" >> ~/.bashrc
+    else
+        echo "~/.profile.sh is already sourced in ~/.bashrc"
+    fi
+}
+
 set_ngrok_token() {
     if [ -z "$NGROK_AUTHTOKEN" ]; then
         echo "Please enter your Ngrok Authtoken:"
         read -r NGROK_AUTHTOKEN
 
-        echo "export NGROK_AUTHTOKEN=\"$NGROK_AUTHTOKEN\"" >> ~/.bashrc
+        echo "export NGROK_AUTHTOKEN=\"$NGROK_AUTHTOKEN\"" >> ~/.profile.sh
         export NGROK_AUTHTOKEN="$NGROK_AUTHTOKEN"
-        echo "Ngrok Authtoken has been set and added to ~/.bashrc"
+        echo "Ngrok Authtoken has been set and added to ~/.profile.sh"
     else
         echo "Ngrok Authtoken is already set: $NGROK_AUTHTOKEN"
         echo "Do you want to change the Authtoken? (y/n):"
@@ -25,7 +34,7 @@ set_ngrok_token() {
             if [ -z "$new_token" ]; then
                 echo "No Authtoken provided. Keeping the old Authtoken: $NGROK_AUTHTOKEN"
             else
-                sed -i "/export NGROK_AUTHTOKEN=/c\export NGROK_AUTHTOKEN=\"$new_token\"" ~/.bashrc
+                sed -i "/export NGROK_AUTHTOKEN=/c\export NGROK_AUTHTOKEN=\"$new_token\"" ~/.profile.sh
                 export NGROK_AUTHTOKEN="$new_token"
                 echo "Ngrok Authtoken has been updated to: $NGROK_AUTHTOKEN"
             fi
@@ -45,9 +54,9 @@ set_ngrok_hostname() {
             exit 1
         fi
 
-        echo "export NGROK_HOSTNAME=\"$NGROK_HOSTNAME\"" >> ~/.bashrc
+        echo "export NGROK_HOSTNAME=\"$NGROK_HOSTNAME\"" >> ~/.profile.sh
         export NGROK_HOSTNAME="$NGROK_HOSTNAME"
-        echo "Ngrok hostname has been set and added to ~/.bashrc"
+        echo "Ngrok hostname has been set and added to ~/.profile.sh"
     else
         echo "Ngrok hostname is already set: $NGROK_HOSTNAME"
         echo "Do you want to change the hostname? (y/n):"
@@ -60,7 +69,7 @@ set_ngrok_hostname() {
             if [ -z "$new_hostname" ]; then
                 echo "No hostname provided. Keeping the old hostname: $NGROK_HOSTNAME"
             else
-                sed -i "/export NGROK_HOSTNAME=/c\export NGROK_HOSTNAME=\"$new_hostname\"" ~/.bashrc
+                sed -i "/export NGROK_HOSTNAME=/c\export NGROK_HOSTNAME=\"$new_hostname\"" ~/.profile.sh
                 export NGROK_HOSTNAME="$new_hostname"
                 echo "Ngrok hostname has been updated to: $NGROK_HOSTNAME"
             fi
@@ -99,6 +108,10 @@ copy_files_to_config_dir() {
 }
 
 # setup
+source ~/.profile.sh
+
+add_profile_to_bashrc
+
 set_ngrok_token
 set_ngrok_hostname
 install_script
@@ -106,4 +119,5 @@ copy_files_to_config_dir
 
 echo "Setup completed successfully!"
 sleep 2
-source ~/.bashrc
+
+source ~/.profile.sh
