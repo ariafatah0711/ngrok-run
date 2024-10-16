c711cd14fe0c4413d67cee3fe03b24e9fa25737c
Aria Fatah <ariafatah0711@gmail.com>
2024-10-16 18:50:07 +0700

    update

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 SCRIPT_NAME="ngrok-run"
 TARGET_PATH="/usr/local/bin"
+CONFIG_DIR="/etc/ngrok-run"
+SOURCE_DIR="$(pwd)"  # Direktori saat ini tempat skrip berada
 
 set_ngrok_token() {
     if [ -z "$NGROK_AUTHTOKEN" ]; then
@@ -49,8 +51,23 @@ install_script() {
     echo "$SCRIPT_NAME has been installed and is now executable globally."
 }
 
+copy_files_to_config_dir() {
+    echo "Copying files to $CONFIG_DIR..."
+
+    # Buat direktori /etc/ngrok-run jika belum ada
+    if [ ! -d "$CONFIG_DIR" ]; then
+        echo "Creating $CONFIG_DIR..."
+        sudo mkdir -p "$CONFIG_DIR"
+    fi
+
+    # Salin semua file dari folder saat ini ke /etc/ngrok-run
+    sudo cp -r "$SOURCE_DIR"/* "$CONFIG_DIR/"
+
+    echo "All files have been copied to $CONFIG_DIR."
+}
+
+# Panggil fungsi-fungsi di dalam skrip
 set_ngrok_token
-
 set_ngrok_hostname
-
 install_script
+copy_files_to_config_dir
